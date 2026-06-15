afaace96fbad029e133f59a3bbc49cf1f7a53788
nimkibet <nimrodkibet376@gmail.com>
2026-06-15 15:26:33 +0300

    fix: update WhatsApp Web pinned version and cache remotePath to resolve 404 error

--- src/config/whatsapp.js
+++ src/config/whatsapp.js
@@ -123,9 +123,10 @@ export const initWhatsAppClient = async () => {
       dataPath: sessionPath
     }),
     puppeteer: puppeteerConfig,
+    webVersion: '2.3000.1041450038-alpha',
     webVersionCache: {
       type: 'remote',
-      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/{version}.html',
     }
   });
 
